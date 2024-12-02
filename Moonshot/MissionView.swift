ee013d52c9ec7e4c841ec563a9edd1507b15dd50
Mark Santoro <mark_santoro@comcast.net>
2024-12-02 11:21:37 -0800

    add mission details view

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -8,7 +8,25 @@
 import SwiftUI
 
 struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
     let mission: Mission
+    let crew: [CrewMember]
+    
+    init(mission: Mission, astronauts: [String: Astronaut]){
+        self.mission = mission
+        
+        self.crew = mission.crew.map {member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
     
     var body: some View {
         ScrollView{
@@ -20,7 +38,11 @@ struct MissionView: View {
                         width * 0.6
                         
                     }
-                    .padding(.top)
+                    
+                Rectangle()
+                    .frame(height: 2)
+                    .foregroundStyle(.lightBackground)
+                    .padding(.vertical)
                 
                 VStack(alignment: .leading){
                     Text("Mission Highlights")
@@ -29,8 +51,53 @@ struct MissionView: View {
                     
                     Text(mission.description)
                     
+                    Rectangle()
+                        .frame(height: 2)
+                        .foregroundStyle(.lightBackground)
+                        .padding(.vertical)
+                    
+                    Text("Crew")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
                 }
-                .padding(.bottom)
+                .padding(.horizontal)
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack{
+                        ForEach(crew, id: \.role) { crewMember in
+                            NavigationLink {
+                                Text("Astronaut details")
+                            } label: {
+                                HStack {
+                                        Image(crewMember.astronaut.id)
+                                            .resizable()
+                                            .frame(width: 104, height: 72)
+                                            .clipShape(.capsule)
+                                            .overlay(
+                                                Capsule()
+                                                    .strokeBorder(.white, lineWidth: 1)
+                                            )
+                                    
+                                    VStack(alignment: .leading){
+                                        Text(crewMember.astronaut.name)
+                                            .foregroundStyle(.white)
+                                            .font(.headline)
+                                        Text(crewMember.role)
+                                            .foregroundStyle(.white.opacity(0.5))
+                                        
+                                    }
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                        
+                        
+                        
+                        
+                    }
+                }
+                
             }
             .navigationTitle(mission.displayName)
             .navigationBarTitleDisplayMode(.inline)
@@ -38,12 +105,14 @@ struct MissionView: View {
             
             
         }
+        
     }
 }
 
     #Preview {
         let missions: [Mission] = Bundle.main.decode("missions.json")
+        let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
         
-        return MissionView(mission: missions[0])
+        return MissionView(mission: missions[3], astronauts: astronauts)
             .preferredColorScheme(.dark)
     }
